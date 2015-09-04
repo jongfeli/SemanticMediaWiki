6a78fab4689ccf3b7b6111e1fe6519d9c4aaefb8
mwjames <jamesin.hongkong.1@gmail.com>
2015-09-04 15:38:55 +0200

    Add TurtleTriplesBuilder::getChunkedTriples, refs 1110
    Based on the work done by Laurent Alquier.

--- src/SPARQLStore/SPARQLStore.php
+++ src/SPARQLStore/SPARQLStore.php
@@ -199,6 +199,8 @@ class SPARQLStore extends Store {
 			new RedirectLookup( $this->getConnection() )
 		);
 
+		$turtleTriplesBuilder->setTriplesChunkSize( 80 );
+
 		if ( !$turtleTriplesBuilder->hasTriplesForUpdate() ) {
 			return;
 		}
@@ -207,10 +209,12 @@ class SPARQLStore extends Store {
 			$this->doSparqlDataDelete( $semanticData->getSubject() );
 		}
 
-		$this->getConnection()->insertData(
-			$turtleTriplesBuilder->getTriples(),
-			$turtleTriplesBuilder->getPrefixes()
-		);
+		foreach( $turtleTriplesBuilder->getChunkedTriples() as $chunkedTriples ) {
+			$this->getConnection()->insertData(
+				$chunkedTriples,
+				$turtleTriplesBuilder->getPrefixes()
+			);
+		}
 	}
 
 	/**
